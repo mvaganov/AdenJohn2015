d1bcfe50ee6020c8027f7183834b2a426f8b5498
Michael Vaganov <mvaganov@hotmail.com>
2015-04-27 16:18:07 -0700

    zoning!

---
+++ AdenJohn_Game/Assets/Michael/script/ZoneTo.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ZoneTo : MonoBehaviour {
+
+	public string nextLevel;
+
+	void OnTriggerEnter(Collider other) {
+		if(other.GetComponent<UnityStandardAssets.Characters.FirstPerson.FirstPersonController>()) {
+			Application.LoadLevel(nextLevel);
+		}
+	}
+}
