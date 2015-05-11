6df8ef27c314036b63d65458c58066f6298f70a2
Michael Vaganov <mvaganov@hotmail.com>
2015-05-11 16:46:24 -0700

    moving guy and weird spiral

---
+++ AdenJohn_Game/Assets/Michael/script/MovingGuy.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovingGuy : MonoBehaviour {
+
+	Rigidbody rb;
+	public float speed = 10;
+
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+	}
+	
+	void Update () {
+		if(Vector3.Dot(rb.velocity, Vector3.down) < 0.1f) {
+			rb.velocity = transform.forward * speed;
+			transform.Rotate(Vector3.up, Random.Range(-360, 360) * Time.deltaTime);
+		}
+	}
+}
