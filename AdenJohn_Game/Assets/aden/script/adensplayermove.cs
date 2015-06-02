b61811e75000e2a79d03b8ad7ab9b00b2749376c
adenrocks1013 <ajschopen@mpcsd.org>
2015-06-01 17:01:07 -0700

    added new charector

---
+++ AdenJohn_Game/Assets/aden/script/adensplayermove.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class adensplayermove : MonoBehaviour {
+
+	public float xSensitivity = 10;
+	// Use this for initialization
+	void Start () {
+		GetComponent<Rigidbody> ().freezeRotation = true;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float mouseX = Input .GetAxis ("Mouse X");
+		transform.Rotate (0, mouseX * xSensitivity, 0);
+	
+		float moveForward = Input.GetAxis ("Vertical");
+	    if (moveForward != 0) {
+			GetComponent<Rigidbody> ().velocity = transform.forward * moveForward;
+		}
+	}
+}
